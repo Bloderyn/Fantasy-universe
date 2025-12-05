f18757e8ee2d783600292ecda0dbaf8ff42a5ea7
Bloderyn <Mtieres@gmail.com>
2025-12-05 17:31:45 +0100

    First codes for main page - first try of implanting JS automation to HTML categories

---
+++ main page/main-page-script.js
@@ -0,0 +1,30 @@
+const date = {
+    nav: [
+        { label: "MAPS", href: "#maps" },
+        { label: "ADVENTURES", href: "#adventures" },
+        { label: "NOVELS", href: "#novels" },
+        { label: "TIMELINE", href: "#timeline" }
+    ],
+
+    codex: [
+        { id: "characters",
+            title: "CHARACTERS",
+            img: "images/characters.png",
+            href: "",
+            description: "Biographies of major and minor characters inhabiting this fantasy universe."
+        },
+        { id: "races", 
+            title: "RACES",
+            img: "images/races.png",
+            href: "",
+            description: "Insights into the species that shape civilizations, conflicts, and myths.",
+        },
+        { id: "society",
+            title: "SOCIETY",
+            img: "images/society.png",
+            href: "",
+            description: "Insights into the social systems that shape kingdoms, tribes and empires.",
+        },
+
+    ]
+}
